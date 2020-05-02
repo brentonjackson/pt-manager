f6002df1ca3fcd6a0d0b8e334928df95800b3f5a
Brentonius <bjavonjackson@gmail.com>
2020-05-02 19:15:00 -0400

    connected form to logic and added slidervalue.js to display form slider value

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,15 @@
 (function (window) {
     'use strict';
+    var FORM_SELECTOR = '[data-training-session="form"]';
     var App = window.App;
     var Gym = App.Gym;
     var DataStore = App.DataStore;
+    var FormHandler = App.FormHandler;
     var myGym = new Gym('midtown', new DataStore());
     window.myGym = myGym;
+    var formHandler = new FormHandler(FORM_SELECTOR);
+    
+    formHandler.addSubmitHandler(myGym.createSession.bind(myGym));
+    console.log(formHandler);
 })(window);
 
