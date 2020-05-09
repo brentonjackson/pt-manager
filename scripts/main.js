199c1210f0e16282383029061190238907fa5e5e
Brentonius <bjavonjackson@gmail.com>
2020-05-09 10:43:31 -0400

    added remote data storage functionality

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,16 @@
     'use strict';
     var FORM_SELECTOR = '[data-training-session="form"]';
     var CHECKLIST_SELECTOR = '[data-training-session="checklist"]';
+    var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
     var App = window.App;
     var Gym = App.Gym;
     var DataStore = App.DataStore;
+    var RemoteDataStore = App.RemoteDataStore;
     var FormHandler = App.FormHandler;
     var Validation = App.Validation;
     var CheckList = App.CheckList;
-    var myGym = new Gym('midtown', new DataStore());
+    var remoteDS = new RemoteDataStore(SERVER_URL);
+    var myGym = new Gym('midtown', remoteDS);
     window.myGym = myGym;
     var checkList = new CheckList(CHECKLIST_SELECTOR);
     checkList.addClickHandler(myGym.completeSession.bind(myGym));
